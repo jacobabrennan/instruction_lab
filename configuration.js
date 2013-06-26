3023ac023f1185b21568d88f8910f4331830de60
michaelsmccarthy <drawnalong@gmail.com>
2013-06-26 00:30:02 -0400

    edited configuration file - deep dive finished

--- configuration.js
+++ configuration.js
@@ -279,63 +279,59 @@ var configuration = {
             }
         },
         instructions: [
-            {unnumbered: true, title: 'Software Defined Networking', time_in: 13,
-                notes: "",
+            {unnumbered: true, title: 'Software Defined Networking', time_in: 41,
+                notes: "In this deep dive into software defined networking, we focus strongly on OpenFlow. Get started by checking out OpenFlow.org. Also, it will be helpful to learn more about the Open Networking Foundation, the organization that manages the OpenFlow standard. Martin Casado of Stanford University maintains a large list of active OpenFlow projects. Check that out as well!",
                 content: [
-                    {type: '', title: '', time_offset: 2, content: {url: ''}},
-                    {type: '', title: '', time_offset: 10, content: {url: ''}},
-                    
+                    {type: 'link', title: 'OpenFlow.org', time_offset: 2, content: {url: 'http://www.openflow.org/'}},
+		    {type: 'link', title: 'Open Networking Foundation', time_offset: 7, content: {url: 'https://www.opennetworking.org/'}},
+		    {type: 'link', title: "Martin Casado's OpenFlow List", time_offset: 14, content: {url: 'http://yuba.stanford.edu/~casado/of-sw.html/'}}
                 ]
             },
             {title: 'Yiannis Yiakoumis', short_title: 'Yiannis Yiakoumis', time_in: 63,
-                notes: "",
+                notes: "Yiannis is a PhD candidate at the Department of Electrical Engineering at Stanford University. Stanford is very active in OpenFlow and SDN research, and a link to their OpenFlow Dashboard is enclosed.",
                 content: [
-		    {type: 'link', title: 'Stanford Research', time_offset: 8, content: {url: 'http://www.stanford.edu/~yiannisy/cgi-bin/research.php'}},
-                    {type: 'link', title: 'Twitter', time_offset: 8, content: {url: 'https://twitter.com/gyiakoumis'}},
-                    {type: 'link', title: 'Github', time_offset: 12, content: {url: 'https://github.com/yiannisy'}}
+		    {type: 'link', title: 'Stanford Research', time_offset: 2, content: {url: 'http://www.stanford.edu/~yiannisy/cgi-bin/research.php'}},
+		    {type: 'link', title: 'OpenFlow at Stanford', time_offset: 5, content: {url: 'https://openflow.stanford.edu/dashboard.action'}},
+		    {type: 'link', title: 'Home Authentication', time_offset: 12, content: {url: 'http://www.multichannel.com/mobile/cable-show-2012-stanford-team-wins-app-challenge-bandwidth-priority-system/125686'}},
+                    {type: 'github', title: 'Github', time_offset: 18, content: {url: 'https://github.com/yiannisy'}}
                 ]
             },
-            {title: '', short_title: '', time_in: 45,
-                notes: "",
+            {title: 'Jason Parraga', short_title: 'Jason Parraga', time_in: 256,
+                notes: "Jason is a student at Marist College. IBM and Marist are involved in an ongoing joint study that began in 1988. Learn more about their partnership, and check out some of Jason's work.",
                 content: [
-                    {type: 'link', title: '', time_offset: 17, content: {url: ''}},
-                    {type: 'link', title: '', time_offset: 19, content: {url: ''}}
+		    {type: 'link', title: 'IBM/Marist Joint Study', time_offset: 7, content: {url: 'http://www.marist.edu/community/ibm.html'}},
+                    {type: 'github', title: 'Github', time_offset: 20, content: {url: 'https://github.com/Sovietaced'}},
+		    {type: 'link', title: 'Access Control List', time_offset: 66, content: {url: 'http://en.wikipedia.org/wiki/Access_control_list'}}
                 ]
             },
-            {title: 'HOOK UP YOUR LED & LOCATE ITS NUMBER IN LINUX', short_title: 'HOOK UP AND LOCATE LED', time_in: 80,
-                notes: "Watch the video carefully and set up your LED. Next, use the Beaglebone manual to discover your pin's default configuration. If you want to set up your own configuration or learn more about pinmuxing on the Beaglebone in general, check out these additional resources.",
+            {title: 'OpenFlow Controllers', short_title: 'OpenFlow Controllers', time_in: 338,
+                notes: "OpenFlow is currently the defacto standard in Software Defined Networking. There are already many good OpenFlow controllers available, so programmers have their pick of resources. Two good examples are Floodlight and Pox. We use Pox, a python-based controller, in our Do-it-yourself SDN Lab.",
                 content: [
-                    {type: '', title: '', time_offset: undefined, content: {url: ''}}
+                    {type: 'link', title: 'Floodlight', time_offset: 14, content: {url: 'http://www.projectfloodlight.org/floodlight/'}},
+		    {type: 'link', title: 'Pox', time_offset: 18, content: {url: 'http://www.noxrepo.org/pox/about-pox/'}}
                 ]
             },
-            {short_title: '', title: '', time_in: undefined,
-                notes: "",
+            {title: 'Jonathan Heiliger', short_title: 'Jonathan Heiliger', time_in: 365,
+                notes: "Jonathan successfully oversaw the expansion of Facebook from forty million, to eight-hundred million users. Pay particular attention to Jonathan's assertion that Ops and Software Engineer disciplines will begin to merge and shift thanks to SDN. Below, you'll find links to help you explore more resources to scaffold your SDN and Gigabit software development.",
                 content: [
-                    {type: '', title: '', time_offset: undefined, content: {url: ''}}
+		    {type: 'link', title: 'GENI', time_offset: 73, content: {url: 'http://www.geni.net/'}},
+		    {type: 'link', title: 'GENI Docs', time_offset: 78, content: {url: 'http://geni-app-developer-documentation.readthedocs.org/en/latest/index.html'}},
+		    {type: 'link', title: 'Big Switch', time_offset: 118, content: {url: 'http://www.bigswitch.com/'}},
+		    {type: 'video', title: 'Hyperglance', time_offset: 152, content: {url: 'http://www.youtube.com/watch?v=L-ad0Phy0eI'}}
                 ]
             },
-            {short_title: '', title: '', time_in: undefined,
-                notes: "",
+            {title: 'Rick Kagan', short_title: 'Rick Kagan', time_in: 526,
+                notes: "Rick's points regarding the inefficiencies of current networking technology should hammer home the new frontier that SDN and OpenFlow creates. Be sure to check out the DIY Software Defined Networking Lab!",
                 content: [
-                    {type: '', title: '', time_offset: undefined, content: {url: ''}}
+                    {type: 'link', title: 'Skype Protocol', time_offset: 43, content: {url: 'http://en.wikipedia.org/wiki/Skype_protocol'}}
                 ]
             },
-            {short_title: '', title: '', time_in: undefined,
-                notes: "",
+            {unnumbered: true, title: 'Dive In!', short_title: '', time_in: 619,
+                notes: "note notes notes",
                 content: [
-                    {type: '', title: '', time_offset: undefined, content: {url: ''}}
-                ]
-            },
-            {short_title: '', title: '', time_in: undefined,
-                notes: "",
-                content: [
-                    {type: '', title: '', time_offset: undefined, content: {url: ''}}
-                ]
-            },
-            {short_title: '', title: '', time_in: undefined,
-                notes: "",
-                content: [
-                    {type: '', title: '', time_offset: undefined, content: {url: ''}}
+                    {type: 'github', title: 'Learning Labs', time_offset: 21, content: {url: 'https://github.com/mozilla/mozilla-ignite-learning-lab-demos'}},
+		    {type: 'link', title: 'Mozilla Ignite Twitter', time_offset: 23, content: {url: 'https://twitter.com/MozillaIgnite'}},
+		    {type: 'link', title: 'Email: Mozilla Ignite', time_offset: 24, content: {url: 'ignite@mozillafoundation.org'}}
                 ]
             }
         ]
